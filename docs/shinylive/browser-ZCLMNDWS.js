310fdee7a7b6c8691b5a232c33455000cad4e17c
Siddhu-S <32528064+Siddhu-S@users.noreply.github.com>
2026-08-20 19:13:12 +0000

    Rebuild GitHub Pages (shinylive export)

--- docs/shinylive/browser-ZCLMNDWS.js
+++ docs/shinylive/browser-ZCLMNDWS.js
@@ -1,8 +1,8 @@
-// Shinylive 0.10.8
+// Shinylive 0.10.14
 // Copyright 2026 Posit, PBC
 import {
   __commonJS
-} from "./chunk-DC4APELQ.js";
+} from "./chunk-PCA63ASY.js";
 
 // node_modules/ws/browser.js
 var require_browser = __commonJS({
